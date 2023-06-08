b8dc085462543e07692147080061f410d8bc4b24
Elizatoria <Elizatoria@gmail.com>
2023-06-08 00:31:04 -0400

    Finished

---
+++ exercises/01-jsx/src/components/Me/Me.jsx
@@ -0,0 +1,5 @@
+const Me = () => {
+  return <div>My name is Victoria Forrest</div>;
+};
+
+export default Me;
--- exercises/01-jsx/src/components/Person/Person.jsx
+++ exercises/01-jsx/src/components/Person/Person.jsx
@@ -10,6 +10,10 @@ const Person = () => {
    * You can get today's year with:
    * @example new Date(Date.now()).getFullYear()
    */
+  const getAge = (birthYear) => {
+    const thisYear = new Date(Date.now()).getFullYear();
+    return thisYear - birthYear;
+  };
 
   return (
     <div>
@@ -19,7 +23,7 @@ const Person = () => {
       {/* Using JSX, plugin occupation below from the person object */}
       <div>Occupation: {person.occupation}</div>
       {/* Using JSX, plugin the birth year using a function that converts age to birth year */}
-      <div>Age: _____</div>
+      <div>Age: {getAge(person.birthYear)}</div>
     </div>
   );
 };
