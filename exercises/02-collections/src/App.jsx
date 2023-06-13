d032143e9f452fc26cd7d533db40c6607e464723
Elizatoria <Elizatoria@gmail.com>
2023-06-12 20:01:31 -0400

    Completing

--- exercises/02-collections/src/App.jsx
+++ exercises/02-collections/src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import { phones } from "./images/phones";
 // import something here
+import {technologies} from "./images/technologies";
+import {phones} from "./images/phones";
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return <div className="App">{/* Complete me */<App />}
+  </div>;
 }
 
 export default App;
