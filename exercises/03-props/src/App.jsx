d59cab2a3d56af19a0025e53f6ce16e17582efb6
Elizatoria <Elizatoria@gmail.com>
2023-06-13 01:06:53 -0400

    Finished

--- exercises/03-props/src/App.jsx
+++ exercises/03-props/src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 // import something here
+import Welcome from "./Components/Welcome";
+import Heading from "./Components/Heading";
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
-}
+  return (
+    <div className="App">
+      <Welcome name="Victoria" />
+      <Welcome />
+      <Heading>Welcome to my React App!</Heading>
+    </div>
+  );}
 
 export default App;
