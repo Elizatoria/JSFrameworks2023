244dd984402be3d1902d8c601ed8c8d1273a1400
Elizatoria <Elizatoria@gmail.com>
2023-06-12 23:53:15 -0400

    Finished

--- exercises/02-collections/src/App.jsx
+++ exercises/02-collections/src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
 import { phones } from "./images/phones";
 // import something here
-import {technologies} from "./images/technologies";
-import {phones} from "./images/phones";
+import { technologies } from "./images/technologies";
 
 function App() {
-  return <div className="App">
-    {technologies.map((image, index) => (
-    <img src={image} key={`technology-${index}`} />
-  ))}
-  {Object.values(phones).map((image, index) => (
-    <img src={image} key={`phone-${index}`} />
-  ))}
-  </div>;
+  return (
+    <div className="App">
+      {technologies.map((image, index) => (
+        <img src={image} key={`technology-${index}`} alt={"Tech"} />
+      ))}
+      {Object.values(phones).map((image, index) => (
+        <img src={image} key={`phone-${index}`} alt={"Phone"} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
