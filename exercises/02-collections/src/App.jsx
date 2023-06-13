201e669b4b38ef4f1830e0ac622c7c07190be299
Elizatoria <Elizatoria@gmail.com>
2023-06-12 20:06:02 -0400

    Finished

--- exercises/02-collections/src/App.jsx
+++ exercises/02-collections/src/App.jsx
@@ -5,7 +5,13 @@ import {technologies} from "./images/technologies";
 import {phones} from "./images/phones";
 
 function App() {
-  return <div className="App">{/* Complete me */<App />}
+  return <div className="App">
+    {technologies.map((image, index) => (
+    <img src={image} key={`technology-${index}`} />
+  ))}
+  {Object.values(phones).map((image, index) => (
+    <img src={image} key={`phone-${index}`} />
+  ))}
   </div>;
 }
 
