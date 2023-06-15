b296301dc13850e8b7fb0ba1a8c90b5bf504ff90
Elizatoria <Elizatoria@gmail.com>
2023-06-15 01:39:38 -0400

    Finished

---
+++ exercises/04-conditional-rendering/src/Components/AccordionSection.jsx
@@ -0,0 +1,19 @@
+const AccordionSection = (props) => {
+  return (
+    <>
+      <div className={props.isOpen ? "title active" : "title"}>
+        <i className="dropdown icon"></i>
+        {props.title}
+      </div>
+      <div className={props.isOpen ? "content active" : "content"}>
+        <p
+          className={props.isOpen ? "transition visible" : "transition hidden"}
+        >
+          {props.paragraph}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AccordionSection;
