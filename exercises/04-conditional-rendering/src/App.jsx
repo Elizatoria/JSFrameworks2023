25abdbdea7f28a0b51f2a6b21d16fde788c15552
Elizatoria <Elizatoria@gmail.com>
2023-06-14 20:33:12 -0400

    Working On It

--- exercises/04-conditional-rendering/src/App.jsx
+++ exercises/04-conditional-rendering/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import  content  from "./content/accordion";
 
 function App() {
-  return <div class="ui styled accordion">{/* Write code here */}</div>;
+  return <div class="ui styled accordion">{<content />}</div>;
 }
 
 export default App;
