de606da5aa1d71d533b0e56d1bf9e1cd3969b647
Elizatoria <Elizatoria@gmail.com>
2023-06-26 22:26:50 -0400

    Finished Less Text

--- exercises/05-state/src/App.jsx
+++ exercises/05-state/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         text="Fahrenheit and centigrade are two temperature scales in use today. The Fahrenheit scale was developed by the German physicist Daniel Gabriel Fahrenheit . In the Fahrenheit scale, water freezes at 32 degrees and boils at 212 degrees.
 
         The centigrade scale, which is also called the Celsius scale, was developed by Swedish astronomer Andres Celsius. In the centigrade scale, water freezes at 0 degrees and boils at 100 degrees."
-        maxLength={100}
+        maxLength={65}
         />
       </div>
 
--- exercises/05-state/src/components/LessText/LessText.jsx
+++ exercises/05-state/src/components/LessText/LessText.jsx
@@ -6,7 +6,13 @@ const LessText = ({ text = "", maxLength }) => {
     <div>
       <p>{!hidden ? text.substring(0, maxLength).trim() : text}</p>
       <div>
-        <button
+        <button  type="button"
+          className="btn btn-dark mr-2"
+          onClick={() => setHidden(!hidden)}
+          >
+            {hidden ? "Read Less" : "Read More"}
+          </button>
+        {/* <button
           type="button"
           className="btn btn-dark mr-2"
           onClick={() => setHidden(true)}
@@ -19,50 +25,10 @@ const LessText = ({ text = "", maxLength }) => {
           onClick={() => setHidden(false)}
         >
           Read Less
-        </button>
+        </button> */}
       </div>
     </div>
   );
 };
 
-// function LessText () {
-//   const [Less] = useState(true);
-//   if (Less) {
-//     return <div className="container pt-4 pb-4">
-//     <button className="close">More Text</button>
-//     </div>
-//   } else
-//   return <div><button className="open">Less Text</button></div>
-// }
-
 export default LessText;
-
-
-// const commentButton = document.querySelector("#commentButton");
-//   const comments = document.querySelector("#comments");
-
-//   commentButton.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     if (comments.classList.contains("hidden")) {
-//       comments.classList.remove("hidden");
-//       commentButton.textContent = "Hide Comments";
-//     } else {
-//       comments.classList.add("hidden");
-//       commentButton.textContent = "View Comments";
-//   }})
-
-// import { useState } from "react";
-// import picture from "./picture.jpg";
-
-// function Warning() {
-//   const [showWarning, setShowWarning] = useState(true);
-//   if (showWarning) {
-//     return <div className="alert alert-warning" role="alert">
-//         <strong>Warning!</strong> If you dismiss this, you will see a disturbing picture.
-//         <button className="close">
-//           <span>&times;</span>
-//         </button>
-//       </div>
-//   }
-//   else return <img src={picture} width="300" />;
-// }
