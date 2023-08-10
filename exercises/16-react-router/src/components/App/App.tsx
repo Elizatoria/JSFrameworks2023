7933a2ea033116403e131d07aff3e82361bd746f
Elizatoria <Elizatoria@gmail.com>
2023-08-09 23:00:43 -0400

    Working on Category

--- exercises/16-react-router/src/components/App/App.tsx
+++ exercises/16-react-router/src/components/App/App.tsx
@@ -10,6 +10,8 @@
  * First, you will need to import the packages you need from React Router.
  * Then, import some things from React Router.
  */
+import { Routes, Route } from "react-router-dom";
+
 import Home from "../Home/Home";
 import Category from "../Category/Category";
 
@@ -28,7 +30,12 @@ function App() {
       <div className="bg-success text-white pt-1 pb-1 mb-3">
         <h1 className="container h2">NoName E-Commerce</h1>
       </div>
-      <main className="container mb-4">{/* Complete me */}</main>
+      <main className="container mb-4">
+      <Routes>
+        <Route path="/components/Home/Home" element={<Home />} />
+        <Route path="/components/Category/Category" element={<Category />} />
+        </Routes>
+        </main>
     </>
   );
 }
--- exercises/16-react-router/src/index.tsx
+++ exercises/16-react-router/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 // Import something here
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -11,6 +12,8 @@ const root = createRoot(rootElement);
 root.render(
   // Add something here
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
